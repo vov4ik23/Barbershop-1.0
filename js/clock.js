ceb77e44dc1044a14619d0c530a2335ad845d4a4
error <errordelete0@gmail.com>
2025-12-04 18:51:03 +0200

    Добавление JS

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+function tick() {
+  const d = new Date();
+  const m = document.getElementById("minuteHand");
+  const h = document.getElementById("hourHand");
+
+  const degH = 30 * (d.getHours() % 12 + d.getMinutes() / 60);
+  const degM = 6 * (d.getMinutes() + d.getSeconds() / 60);
+
+  m.style.transform = "rotate(" + degM + "deg)";
+  h.style.transform = "rotate(" + degH + "deg)";
+
+  requestAnimationFrame(tick);
+}
+tick();
