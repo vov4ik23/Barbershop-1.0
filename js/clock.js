1263e0fd545a3d44e806d40ed6f8d909a7940257
webby_vova <152632573+vov4ik23@users.noreply.github.com>
2026-01-16 14:07:50 +0200

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,14 +1,14 @@
-function tick() {
-  const d = new Date();
-  const m = document.getElementById("minuteHand");
-  const h = document.getElementById("hourHand");
+// function tick() {
+//   const d = new Date();
+//   const m = document.getElementById("minuteHand");
+//   const h = document.getElementById("hourHand");
 
-  const degH = 30 * (d.getHours() % 12 + d.getMinutes() / 60);
-  const degM = 6 * (d.getMinutes() + d.getSeconds() / 60);
+//   const degH = 30 * (d.getHours() % 12 + d.getMinutes() / 60);
+//   const degM = 6 * (d.getMinutes() + d.getSeconds() / 60);
 
-  m.style.transform = "rotate(" + degM + "deg)";
-  h.style.transform = "rotate(" + degH + "deg)";
+//   m.style.transform = "rotate(" + degM + "deg)";
+//   h.style.transform = "rotate(" + degH + "deg)";
 
-  requestAnimationFrame(tick);
-}
-tick();
+//   requestAnimationFrame(tick);
+// }
+// tick();
