452e75cd7c006450f313b5d3272fd14f4290c0d8
webby_vova <152632573+vov4ik23@users.noreply.github.com>
2026-02-09 14:39:23 +0200

    Delete js directory

--- js/burger.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('.burgerMenu');
-const overlay = document.querySelector('.overlay');
-
-if (btn && overlay) {
-  btn.addEventListener('click', () => {
-    btn.classList.toggle('active');
-    overlay.classList.toggle('overlay--on');
-  });
-}
